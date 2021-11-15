bef51c053070c071ec10b4f99f50041babb9e1ff
LeonidMakhov <makhov-leonid999@yandex.ru>
2021-11-15 21:54:23 +0300

    Update Contacts STAFF two line.js

--- src/Components/Contacts.js
+++ src/Components/Contacts.js
@@ -1,7 +1,8 @@
 function Contacts(props) {
     return (
         <div>
-            <ul><h3> Contacts STAFF </h3></ul>
+            <ul><h3> Contacts </h3></ul>
+            <ul><h3> STAFF </h3></ul>
             <ul> {props.car.map(el => <li key={el.model}> {el.contact} for {el.model}</li>)} </ul>
         </div>
 )
