7346fc0c316aebd65ba00787bfdd2050ed415b14
LeonidMakhov <makhov-leonid999@yandex.ru>
2021-11-15 23:23:36 +0300

    COLOR

--- src/Components/Color.js
+++ src/Components/Color.js
@@ -1,7 +1,7 @@
 function Color (props){
     return (
         <div>
-            <ul> <h3> Color </h3> </ul>
+            <ul> <h3> COLOR </h3> </ul>
             <ul> {props.car.map(el => <li key={el.model}> {el.model} is {el.color} </li>)} </ul>
         </div>
     )
