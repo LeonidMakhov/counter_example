238fd132291b968c2909f9c42eb39dc0609f7409
LeonidMakhov <makhov-leonid999@yandex.ru>
2021-11-15 21:55:29 +0300

    Update Contacts double

--- src/Components/Contacts.js
+++ src/Components/Contacts.js
@@ -3,6 +3,7 @@ function Contacts(props) {
         <div>
             <ul><h3> CONTACTS </h3></ul>
             <ul> {props.car.map(el => <li key={el.model}> {el.contact} for {el.model}</li>)} </ul>
+            <ul><h3> CONTACTS </h3></ul>
         </div>
 )
 }
